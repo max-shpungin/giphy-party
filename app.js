8fde3ad6c29034a621691a07294439cc781be7d5
Max Shpungin <maxsdance@gmail.com>
2023-11-14 17:02:22 -0800

    got to first response

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+"use strict";
+/**
+ * Allow the user to search for a GIF and when the form is submitted,
+ * make an AJAX request to the Giphy API and return a single GIF
+ *
+Once the Giphy API has responded with data, append the GIF to the page
+
+Allow the user to search for as many GIFs as they would like
+and keep appending them to the page
+
+Allow the user to remove all of the GIFs by clicking a button
+
+Here is an example of what the application might look like:
+ */
+
 console.log("Let's get this party started!");
+
+// use fetch to make request to giphy
+// http://api.giphy.com/v1/gifs/search
+// ?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+// console log the response data
+const $form = $('form');
+$form.on('submit',handleForm);
+
+/** Makes the fetch request, appends form input and giphy api key */
+async function makeRequest(){
+  const giphyUrl = 'http://api.giphy.com/v1/gifs/search?'
+  const input = $('input').val();
+  const params = new URLSearchParams(
+    {
+      q:input,
+      api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+    });
+
+    // http://api.giphy.com/v1/gifs/search?
+    // q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+
+  const request = await fetch(giphyUrl+params);
+  const responseObj = await request.json();
+  console.log('fetch response: ',responseObj);
+}
+
+/** Calls async function to make fetch request */
+async function handleForm(evt){
+  evt.preventDefault();
+  makeRequest();
+}
